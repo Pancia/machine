fa5142b7343c9b7cae867211dfa8891a3ebde7c1
Anthony D'Ambrosio <nsa.anthony@gmail.com>
2016-03-01 21:16:20 -0800

    url & license in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject machine "1.0.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Library for reading multiple input tapes with a concept of states borrowed from Finite State Machines."
+  :url "https://github.com/Pancia/machine"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
