03c7011c6e13298504a60612ab7974963a12b4ff
Anthony D'Ambrosio <nsa.anthony@gmail.com>
2016-03-08 19:44:27 -0800

    Oops, depending on itself

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
-(defproject machine "1.0.1"
+(defproject machine "1.0.2"
   :description "Library for reading multiple input tapes with a concept of states borrowed from Finite State Machines."
   :url "https://github.com/Pancia/machine"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [machine "1.0.0"]
                  [navis/untangled-spec "0.3.5" :scope "test"]]
   :jvm-opts ["-XX:-OmitStackTraceInFastThrow"]
   :test-refresh {:changes-only true
